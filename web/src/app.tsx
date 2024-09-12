b3b626436656d635929c5df8b363a293a3688e34
Gustavo Teles <ustavoteles@hmail.com>
2024-09-12 15:15:09 -0300

    feat: create empty goals screen

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,3 +1,23 @@
+import { Plus } from 'lucide-react'
+
+import logo from './assets/logo-in-routine.svg'
+import letsStart from './assets/lets-start.svg'
 export function App() {
-  return <h1 className="bg-black text-white text-3xl">Hello World</h1>
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="routine" />
+      <img src={letsStart} alt="routine" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-[#10b97e] text-[#d2feed] flex items-center gap-2 txt-sm font-medium tracking-tight hover:bg-[#10b97ec1]"
+      >
+        <Plus className="size-4" />
+        Cadastrar meta
+      </button>
+    </div>
+  )
 }
